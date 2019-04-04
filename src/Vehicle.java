e13576ccbb6c591d6b442dde2cbda4cf1ac0f18c
Marc <u1953616@campus.udg.edu>
2019-04-04 18:51:51 +0200

    Classe vehicle creada Amb els seus atributs i metodes corresponents

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -9,8 +9,46 @@
  * @author Marc Padrós Jiménez
  */
 public class Vehicle {
+    
     String matricula;
-    String nom;
-    int places;
-    String paco;
+    String marca;
+    String model;
+    int autonomia;          // en km 
+    float consum;           // Kwh/100km 
+    int duradaCarrega;      // hores / minuts (API temps Java)
+    int nPlaces;
+    
+    // invariant: nPlaces > 1
+    //            autonomia > 0
+    //            consum > 0 
+    //            duradaCarrega > 0 
+    
+    // Excepcions: el main podria llançar una excepcio si el temps de carrega es excessiu per indicar que el vehicle no es pot acceptar 
+    
+    public int getAutonomia() {
+        
+        return autonomia; 
+        
+    }
+    
+    public float getConsum() {
+        
+        return consum; 
+        
+    }
+    
+    public int getDuradaCarrega() {
+        
+        return duradaCarrega;
+        
+    }
+    
+    public int getNombreplaces() {
+        
+        return nPlaces;
+        
+    }
+    
+    
+    
 }
