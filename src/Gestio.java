994d82acbc0a99f0ba1c53721d742e8f813988d9
marcp <marcp@DESKTOP-78REKPJ.home>
2019-04-22 03:15:14 +0200

    Classe gestió modificada S'han afegit CrearVehicles,CrearLoc,CrearConn

--- src/Gestio.java
+++ src/Gestio.java
@@ -43,12 +43,17 @@ public class Gestio {
             System.err.println("Fitxer no existeix \n");
         }
         
+    
         
-        // Llegim el fitxer de vehicles 
-        // El try catch s'ha de fer al main no al mètode
-        Scanner teclat = new Scanner(System.in);
+    }
+    
+    
+    public void CrearVehicles() {
+        
         System.out.println("Fitxer de vehicles: ");
-        Scanner fitxerVehicles = new Scanner(new File(teclat.nextLine()));
+        Scanner teclat = new Scanner(System.in);
+        File fitVeh = new File(teclat.nextLine()); 
+        Scanner fitxerVehicles = new Scanner(fitVeh);
         teclat.close();
         while(fitxerVehicles.hasNext()){
             String matricula = fitxerVehicles.next();
@@ -64,8 +69,74 @@ public class Gestio {
         }
         
         fitxerVehicles.close();
+          
+    }
     
+    public void CrearLocalitzacions(Mapa m) {
+        
+        System.out.println("Fitxer de localitzacions: ");
+        Scanner teclat = new Scanner(System.in);
+        File fitLoc = new File(teclat.nextLine()); 
+       
+        Scanner fitxerLoc = new Scanner(fitLoc);
+        while (fitxerLoc.hasNextLine()) {
+            int iden = fitxerLoc.nextInt();
+            String nom = fitxerLoc.next();
+            int popul = fitxerLoc.nextInt(); 
+            boolean puntRecarr = false; 
+            if (nom == "PC") {      // Si la localització és un punt de recàrrega 
+                puntRecarr = true;
+                int nPlaces = fitxerLoc.nextInt();
+                String carrRapid = fitxerLoc.next();
+                boolean carregaRapida = false; 
+                if (carrRapid == "SI")
+                    carregaRapida = true;
+                
+                Localitzacio puntRec = new PuntDeRecarrega(iden,nom,popul,nPlaces,carregaRapida); 
+                m.AfegirLocalitzacio(puntRec);
+            }
+            else { 
+                Localitzacio loc = new Localitzacio(iden,nom,popul);   
+                m.AfegirLocalitzacio(loc);
+            }
+        }
         
     }
+    
+    public void CrearConnexions(Mapa m) {
+        
+        System.out.println("Fitxer de connexions: ");
+        Scanner teclat = new Scanner(System.in);
+        File fitCon = new File(teclat.nextLine()); 
+       
+        Scanner fitxerCon = new Scanner(fitCon);
+        while (fitxerCon.hasNextLine()) {
+            int origen = fitxerCon.nextInt();
+            int desti = fitxerCon.nextInt();
+            int tempsEstimat = fitxerCon.nextInt();
+            float distKm = fitxerCon.nextFloat(); 
+            m.AfegirConnexio(origen, desti, distKm, tempsEstimat);
+        }
+        
+    }
+    
+    public void CrearMapa() {
+        
+        Mapa m = new Mapa(); 
+        
+        CrearLocalitzacions(m);
+        CrearConnexions(m);
+            
+    }
+    
+    public void CrearPeticions() {
+        
+        
+        
+    }
+        
+        
+    
+    
     
 }
