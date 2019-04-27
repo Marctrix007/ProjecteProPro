09094158c9784923469d7b36f3b49a2f8ea6e710
QuiqueSG <49246479+QuiqueSG@users.noreply.github.com>
2019-04-27 19:18:09 +0200

    Punt de Recarrega definitiu
    Excepcions i pres i posts afegides, comprovació dels mètodes feta, invariant d'atributs, mètode toString afegit, new del HashMap fet al constructor amb parametres, constructor per defecte afegit.

--- src/PuntDeRecarrega.java
+++ src/PuntDeRecarrega.java
@@ -1,5 +1,9 @@
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -13,52 +17,70 @@ import java.util.HashSet;
  */
 public class PuntDeRecarrega extends Localitzacio {
     
-    
+    PuntDeRecarrega(){
+        super();
+        a_nPlaces = 0;
+        a_carregaRapida = false;
+        a_parking = new HashMap();
+    }
     
     PuntDeRecarrega(String nom, int index_popularitat, int nPlaces, boolean CarregaRapida){
         super(nom,index_popularitat);
         a_nPlaces = nPlaces;
         a_carregaRapida = CarregaRapida;
+        a_parking = new HashMap();
     }
     
-    
-    public boolean acceptaCarregaRapida(){
+    public boolean AcceptaCarregaRapida(){
+    //Pre: a_nPlaces > mida a_parking
+    //Post: retorna el nombre de places lliures al PC        
         return a_carregaRapida;
     }
     
-    public int places_lliures(){
+    public int PlacesLliures() throws ExcepcioNoQuedenPlaces{
+    //Pre: a_nPlaces > mida a_parking
+    //Post: retorna el nombre de places lliures al PC
         return (a_nPlaces - a_parking.size());
     }
   
     
-    public void estacionar_vehicle(Vehicle v, Temps tempsEntrada){
-      
-        if ( places_lliures() == 0))
-           throw("No queden places lliures"); 
+    public void EstacionarVehicle(Vehicle v, Temps tempsEntrada) throws ExcepcioNoQuedenPlaces{
+     /*
+        Pre: han de haver places al punt de recarrega
+        Post: vehicle v estacionat conjuntament amb el tempEntrada + tCar
+    
+    */      
+        if(this.PlacesLliures() == 0)
+           throw new ExcepcioNoQuedenPlaces("No queden places lliures"); 
         else{
-            Temps tCar = v.tempsCarrega();
-            if (acceptaCarregaRapida() && v.carregaRapida())
-                tCar = tCar.per(0.7);
-            a_parking.put(v, tempEntrada.mes(tCar));
+            Temps tCar = v.TempsCarrega();
+            if (AcceptaCarregaRapida() && v.CarregaRapida())
+                tCar = tCar.per((float) 0.7);
+            a_parking.put(v, tempsEntrada.mes(tCar));
         }
     }
     
-    public Vehicle sortida_vehicle(int recorregut, int nPersones, Temps tempsSortida){    // vehicle no surt fins que no està carregat 
-        
-        if (a_parking.isEmpty())
-            throw("No hi ha vehicles");
+    public Vehicle SortidaVehicle(int recorregut, int nPersones, Temps tempsSortida) throws ExcepcioNoQuedenVehicles{    // vehicle no surt fins que no està carregat 
+    /*
+        Pre: recorregut > 0, nPersones > 0 i han de haver vehicles estacionats al punt de recarrega
+        Post: surt un Vehicle v qualsevol que la seva autonomia > recorregut, NombrePlaces > nPersones i que tempsCarrega < tempsSortida
+    
+    */    
+        if (a_parking.isEmpty()){
+            throw new ExcepcioNoQuedenVehicles("No hi ha vehicles");
+        }
         else{
         
             Set<Map.Entry<Vehicle, Temps>> vehicles = a_parking.entrySet();
             Iterator<Map.Entry<Vehicle, Temps>> it_vehicles = vehicles.iterator();
             Map.Entry<Vehicle, Temps> entry_vehicle;
-            Vehicle v;
+            Vehicle v = null;
             boolean trobat = false;
             
             while(it_vehicles.hasNext() && !trobat){
                 entry_vehicle = it_vehicles.next();
                 v = entry_vehicle.getKey();
-                if (v.autonomia() > recorregut && v.nPlaces() > nPersones && entry_vehicle.getValue().inferior(tempsSortida)){
+                if (v.Autonomia() > recorregut && v.NombrePlaces() > nPersones && entry_vehicle.getValue().compareTo(tempsSortida) < 0){
                     trobat = true;
                 }
             }
@@ -71,20 +93,69 @@ public class PuntDeRecarrega extends Localitzacio {
             }
             
         }
+    }    
         
         
-        
-    }
+    
     
     @Override
     public boolean esPuntDeRecarrega(){
         return true;
     }
     
+    @Override
+    public String toString(){
+        String s = "\n-------------------------------------------\n";
+        try {
+            s = s + "Nom del punt: " + a_nom + "\nTe popularitat: " + a_index_pop + "\nPlaces disponibles: " + PlacesLliures();
+        } catch (ExcepcioNoQuedenPlaces ex) {
+            System.err.println(ex);
+        }
+        
+        Set<Map.Entry<Vehicle, Temps>> vehicles = a_parking.entrySet();
+        Iterator<Map.Entry<Vehicle, Temps>> it_vehicles = vehicles.iterator();
+        Map.Entry<Vehicle, Temps> entry_vehicle;
+        s = s + "\n*****************************";
+        s = s + "\nVehicles que conté:\n";
+        while(it_vehicles.hasNext()){
+            entry_vehicle = it_vehicles.next();
+            Vehicle v = entry_vehicle.getKey();
+            s = s + v.toString()+"\n\n";
+        
+        }
+        s = s + "\n-------------------------------------------\n";
+        return s;
+    }
     
-    private int a_nPlaces;
-    private boolean a_carregaRapida;
-    private HashMap<Vehicle, Temps> a_parking;
+    //ATRIBUTS
+    private final int a_nPlaces; //INVARIANT: a_nPlaces > 0 i a_nPlaces > a_parking
+    private final boolean a_carregaRapida; //INVARIANT: cert
+    private HashMap<Vehicle, Temps> a_parking; //INVARIANT: cert
+    // Més el de la classe Localització heretats
+    
+    
+
+//------------------------------------------------------------------------------------------------------    
+    //EXCEPCIONS
+    private static class ExcepcioNoQuedenPlaces extends Exception {
+    //Descripcio: excepcio que es crida quan no queden places al parking i no es poden afegir més vehicles  
+        public ExcepcioNoQuedenPlaces(){
+            super();
+        }
+        public ExcepcioNoQuedenPlaces(String s) {
+            super(s);
+        }
+    }
+
+    private static class ExcepcioNoQuedenVehicles extends Exception {
+    //Descripcio: excepcio que es crida quan no hi ha vehicles i es demana que en surti un
+        public ExcepcioNoQuedenVehicles() {
+            super();
+        }
+        public ExcepcioNoQuedenVehicles(String s) {
+            super(s);
+        }
+    }
     
     
 }
