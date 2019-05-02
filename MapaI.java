f621c2310a39d4d36fe7bfa4af2c5ab345a05feb
xavirm15 <49242908+xavirm15@users.noreply.github.com>
2019-05-02 23:04:33 +0200

    Interface de Mapa

--- MapaI.java
+++ MapaI.java
@@ -24,11 +24,11 @@ public interface MapaI
     
     public PuntDeRecarrega PRmesProximA(int loc) throws Exception;
         //Pre: loc < localitzacions.size()
-        //Post: Retorna la ruta al punt de recàrrega més pròxim a loc
+        //Post: Retorna la ruta al punt de recàrrega més pròxim a loc que tingui places disponibles
 
-    public PuntDeRecarrega PRmesProximDesde(int loc) throws Exception;
+    public PuntDeRecarrega PRmesProximDesde(int loc, int nPlaces, double dist) throws Exception;
         //Pre: loc < localitzacions.size()
-        //Post: Retorna la ruta al punt de recàrrega més pròxim des de loc
+        //Post: Retorna la ruta al punt de recàrrega més pròxim des de loc que disposi d'un vehicle amb nPlaces i autonomia suficeint
         
     public Ruta CamiMinim(int o, int d) throws Exception;
         //Pre: {o,d} < localitzacions.size()
