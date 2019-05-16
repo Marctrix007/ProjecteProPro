a41c5597cdb01206d02ed7f877d6aac65d441313
xavirm15 <49242908+xavirm15@users.noreply.github.com>
2019-05-16 11:45:21 +0200

    Update Temps.java

--- src/Temps.java
+++ src/Temps.java
@@ -1,66 +1,84 @@
 /**
- * @author Xavier Rodríguez i Martínez
+@class Temps
+@brief Temps en hh:mm
+@author Xavier Rodríguez i Martínez
  */
 public class Temps implements Comparable<Temps>{
-    //Descripció general: Classe Temps amb Hora:minut
 
     private final int hora;
     private final int minut;
-
+    
+    /**
+    @pre --
+    @post Constructor per defecte
+    */
     public Temps(){
         hora = 0;
         minut = 0;
     }
-
+    
+    /**
+    @pre --
+    @post Temps creat amb l'excés de minuts corregit
+    */
     public Temps(int h, int m){
-        //Pre: --
-        //Post: Temps creat amb l'excés de minuts corregits
         hora = h + m/60;
         minut = m%60;
     }
 
+    /**
+    @pre --
+    @post L'hora és l'enter i els minuts la fracció centesimal
+    */
     public Temps(float t){
-      //Pre: --
-      //Post: L'hora és l'enter i els minuts la fracció centesimal dels decimals
       hora = (int) t;
       t -= hora;
       minut = (int) t*60;
     }
     
-    public Temps(String s) {
-    // Pre: s té la forma hh:mm 
-    // Post: Temps creat a partir de s
-    
+    /**
+    @pre s  té forma hh:mm
+    @post Temps creat
+    */
+    public Temps(String s) {   
         String[] tempsArr = s.split(":"); 
         hora = Integer.parseInt(tempsArr[0]);
         minut = Integer.parseInt(tempsArr[1]);
         
     }
     
+    /**
+    @pre --
+    @post Retorna el temps de  this  i  t  sumats
+    */
     public Temps mes(Temps t){
-        //Pre: --
-        //Post: Retorna els temps de  this  i  t  sumats.
         return new Temps(this.hora+t.hora, this.minut+t.minut);
     }
 
+    /**
+    @pre --
+    @post Retorna la diferència de  this  i  t
+    */
     public Temps menys(Temps t){
-        //Pre: --
-        //Post: Retora la diferència de  this  i  t.
         int m1 = this.hora*60 + this.minut;
         int m2 = t.hora*60 + t.minut;
         return new Temps(0, m1-m2);
     }
 
-
+    /**
+    @pre --
+    @post Multiplica el temps  this  per  n  vegades
+    */
     public Temps per(float n){
-        //Pre: --
-        //Post: multiplica el temps  this  per  n  vegades
         int m = this.minut + this.hora*60;
         m = (int) (m*n);
         return new Temps(0, m);
     }
     
-    //Metode fet per Quique per estadistics
+    /**
+    @pre --
+    @post Retorna el Temps en format double
+    */
     public double conversioDouble(){
         return hora + minut/60.0;
     }
