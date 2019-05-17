eff827f026c8f4f50e3bc11822452f9f1d279b3e
Marc <u1953616@campus.udg.edu>
2019-05-17 22:53:24 +0200

    act

--- src/Peticio.java
+++ src/Peticio.java
@@ -1,19 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/** @file Peticio.java
+    @brief Classe Peticio
+*/
 
-/**
- *
- * @author Marc Padrós Jiménez
- */
+/** @class Peticio
+    @brief Guarda les dades de la petició 
+    @author Marc Padrós 
+*/
 
 
 
 public class Peticio implements Comparable<Peticio> {
     
-    /* ATRIBUTS */ 
+// -----------------------------------------------
+    
+    /*** ATRIBUTS ***/ 
     int identificador; 
     Temps horaTrucada;
     Temps horaSortida;
@@ -21,8 +21,23 @@ public class Peticio implements Comparable<Peticio> {
     Localitzacio desti; 
     int nClients; 
     
+    // INVARIANT:
+    // identificador > 0
+    // horaTrucada != null
+    // horaSortida != null 
+    // origen != null
+    // desti != null
+    // nClients > 0 
     
-    public Peticio(int identificador, Temps horaTrucada, Temps horaSortida, Localitzacio origen, Localitzacio desti, int nClients) {
+//--------------------------------------------------
+    
+    
+    /**
+     * @brief Creació de la petició  
+     * @pre Cert
+     * @post Crea Peticio amb identificador, horaTrucada, horaSortida, origen, desti i nClients 
+     */
+    Peticio(int identificador, Temps horaTrucada, Temps horaSortida, Localitzacio origen, Localitzacio desti, int nClients) {
         
         this.identificador = identificador;
         this.horaTrucada = horaTrucada;
@@ -32,44 +47,76 @@ public class Peticio implements Comparable<Peticio> {
         this.nClients = nClients;        
         
     }
-
+    
+    /**
+     * @brief Identificador de la petició  
+     * @pre Cert
+     * @post Retorna l'identificador de la petició 
+     */
     public int identificador() {
         return identificador; 
     }
     
+    /**
+     * @brief Hora de recollida de la petició 
+     * @pre Cert
+     * @post Retorna l'hora en la qual la petició demana sortir del punt d'origen 
+     */
     public Temps horaSortida() {
         
         return horaSortida;
         
     }
     
+    /**
+     * @brief Hora en la qual el client truca a l'empresa
+     * @pre Cert
+     * @post Retorna l'hora de trucada de la petició 
+     */
     public Temps horaTrucada() {
-    // Pre: --
-    // Post: Retorna l'hora de trucada de la petició
     
         return horaTrucada; 
+    
+    }
+    
+    /**
+     * @brief Origen de la petició 
+     * @pre Cert
+     * @post Retorna el punt d'origen de la petició 
+     */
+    public Localitzacio origen() {
+        
+        return origen;
+    
+    }
+    
+    /**
+     * @brief Destí de la petició 
+     * @pre Cert
+     * @post Retorna la destinació de la petició 
+     */
+    public Localitzacio desti() {
+        
+        return desti; 
         
     }
     
-    public Localitzacio origen() {
-    // Pre: --
-    // Post: Retorna el punt d'origen de la petició
-        return origen;
-    }
-    
-    
-    public Localitzacio desti() {
-    // Pre: --
-    // Post: Retorna la destinació de la petició
-        return desti; 
-    }
-    
+    /**
+     * @brief Nombre de clients de la petició 
+     * @pre Cert
+     * @post Retorna el nombre de clients que han de ser recollits conjuntament en un punt 
+     */
     public int NombreClients() {
         
         return nClients; 
         
     }
     
+    /**
+     * @brief Comparació de les hores de sortida de 2 peticions  
+     * @pre Cert
+     * @post Retorna un enter més petit que 0 si l'hora de sortida de this és inferior a la de p; si són iguals retorna 0 i si l'hora de sortida de p és superior a la de this retorna un enter més gran que 0 
+     */
     @Override
     public int compareTo(Peticio p) {
         
@@ -77,10 +124,18 @@ public class Peticio implements Comparable<Peticio> {
         
     }
     
+    /**
+     * @brief Dades de la petició 
+     * @pre Cert
+     * @post Mostra les dades de la petició 
+     */
     @Override
     public String toString(){
-        return "PETICIÓ: " + identificador  + "\n\nHora trucada: " + horaTrucada + "\nHora sortida: " + horaSortida + "\nOrigen: " + origen + "\nDesti " + desti  + "\nNombre de clients: " + nClients + "\n";
-
+        
+        String s = "\nPETICIÓ " + identificador  + ":\n\nHora trucada: " + horaTrucada + "\nHora sortida: ";
+        s+= horaSortida + "\nOrigen: " + origen + "\nDestí: " + desti  + "\nNombre de clients: " + nClients + "\n";
+        return s; 
+        
     }
     
 }
