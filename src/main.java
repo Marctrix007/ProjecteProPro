a91bc937c34098e40d723cf9df9eb87d03643898
QuiqueSG <49246479+QuiqueSG@users.noreply.github.com>
2019-05-21 22:44:53 +0200

    try catch

--- src/main.java
+++ src/main.java
@@ -1,12 +1,7 @@
 
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -16,17 +11,35 @@ public class main {
 
     /**
      * @param args the command line arguments
-     * @throws java.io.IOException
      */
-    public static void main(String[] args) throws IOException, Exception {
+    public static void main(String[] args) {
     
         Gestio g = new Gestio(); 
-        g.CrearMapa();
-        g.CrearVehicles();
-        g.CrearPeticions();
-        g.AtendrePeticions();
-        g.MostrarEstadistics();
+        try{
+            g.CrearMapa();
+            g.CrearVehicles();
+            g.CrearPeticions();
+            g.AtendrePeticions();
+            g.MostrarEstadistics();
+        }
+        catch(PuntDeRecarrega.ExcepcioNoQuedenVehicles nqv){
+            System.err.println("No queden vehicles " + nqv);        
+        }
+        catch(PuntDeRecarrega.ExcepcioNoQuedenPlaces nqp){
+            System.err.println("No queden places " + nqp);
+        }
+        catch(FileNotFoundException fnf){
+            System.err.println("Fitxer no trobat: " + fnf);
+        }
         
+        catch(IndexOutOfBoundsException ioob){
+            System.err.println("Ha sortit fora de rang: " + ioob);
+        }
+        catch(Exception e){
+            System.err.println("Excepcio trobada: " + e);
+        }
+        
+       
     }
     
 }
